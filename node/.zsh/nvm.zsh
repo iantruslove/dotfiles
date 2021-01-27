b42cc32d2969be51ffd65418b9e394ebc2f3df39
Ian Truslove <ian.truslove@gmail.com>
2021-01-26 19:41:54 -0700

    Work around new Homebrew locations

--- node/.zsh/nvm.zsh
+++ node/.zsh/nvm.zsh
@@ -2,5 +2,10 @@
 
 export NVM_DIR="$HOME/.nvm"
 
-[ -s "/usr/local/opt/nvm/nvm.sh" ] && . "/usr/local/opt/nvm/nvm.sh"  # This loads nvm
-[ -s "/usr/local/opt/nvm/etc/bash_completion.d/nvm" ] && . "/usr/local/opt/nvm/etc/bash_completion.d/nvm"  # This loads nvm bash_completion
+# Homebrew is changing where it lives, hence this conditional...
+_nvm_dir=""
+[[ -d "/usr/local/opt/nvm/" ]] && _nvm_dir=/usr/local/opt/nvm
+[[ -d "/opt/homebrew/opt/nvm/" ]] && _nvm_dir=/opt/homebrew/opt/nvm
+
+[ -s "$_nvm_dir/nvm.sh" ] && . "$_nvm_dir/nvm.sh"  # This loads nvm
+[ -s "$_nvm_dir/etc/bash_completion.d/nvm" ] && . "$_nvm_dir/etc/bash_completion.d/nvm"  # This loads nvm bash_completion
