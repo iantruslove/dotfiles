aab17d8084c7f309d9bb3113beb0ed7a3690fac3
Ian Truslove <ian.truslove@gmail.com>
2026-01-27 15:55:08 -0800

    Tweak ssh terminal coloring in wezterm

--- main/.wezterm.lua
+++ main/.wezterm.lua
@@ -13,6 +13,7 @@ config.initial_rows = 50
 -- or, changing the font size and color scheme.
 config.font_size = 12
 config.color_scheme = 'Monokai Soda'
+-- config.color_scheme = 'Selenized Dark (Gogh)'
 -- config.color_scheme = 'Modus Vivendi (Gogh)'
 
 config.keys = {
@@ -22,6 +23,22 @@ config.keys = {
     action = wezterm.action.SendString '\n',
   },
 }
+
+wezterm.on(
+  'update-right-status',
+  function(window, pane)
+    local process = pane:get_foreground_process_name() or ''
+    local title = pane:get_title() or ''
+
+    if title:find('ian@nuc') then
+      window:set_config_overrides({
+          color_scheme = 'Sequoia Moonlight',
+      })
+    else
+      window:set_config_overrides({
+      })
+    end
+end)
 
 -- Finally, return the configuration to wezterm:
 return config
